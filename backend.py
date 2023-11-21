a2a32b60f4001cbb94535fe619617dc244448415
nupelem <89153040+nupelem@users.noreply.github.com>
2023-11-21 16:18:59 +0300

    #Batch input via text file/csv/json/etc. transfer:

--- backend.py
+++ backend.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, jsonify
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import auth
-from firebase_admin import db
+from firebase_admin import credentials, db, auth
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 # Initialize Firebase Admin SDK with your credentials
+cred = credentials.Certificate("/Users/nupelem/Desktop/tune-mosaic-firebase-adminsdk-twt4o-076c7f19ae.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://your-firebase-project-id.firebaseio.com/'})
 
-cred = credentials.Certificate("/Users/zeynep/Desktop/tune-mosaic-firebase-adminsdk-twt4o-1a651b2af6.json") 
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://tune-mosaic-default-rtdb.firebaseio.com/'
-})
+# Define the allowed file extensions and the upload folder
+ALLOWED_EXTENSIONS = {'txt', 'csv', 'json'}
+UPLOAD_FOLDER = '/path/to/your/upload/folder'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Function to check if a file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Function to register a new user and store user data in Firebase
 def register_user(email, password, user_data):
@@ -24,52 +31,61 @@ def register_user(email, password, user_data):
 
         # Store user data in the Firebase Realtime Database
         user_id = user.uid
-        database = firebase_admin.db.reference()
-        database.child('users').child(user_id).set( user_data)
+        database = db.reference()
+        database.child('users').child(user_id).set(user_data)
 
         return {'success': True, 'message': 'User registered successfully'}
     except Exception as e:
         return {'success': False, 'message': str(e)}
 
-def retrieve_user_data(email):
-    ref = db.reference('users')
-    users = ref.order_by_child('email').equal_to(email).get()
-
-    for user_id, user_data in users.items():
-        if user_data['email'] == email:
-            return user_data
-
-    return None  # User not found
-    
-def login_user(email, password):
+# Function to add a single song manually
+def add_song(user_id, song_data):
     try:
-        # Retrieve the user by email
-        user = auth.get_user_by_email(email)
+        # Add the song data to the user's collection in the Firebase Realtime Database
+        database = db.reference()
+        songs_ref = database.child('users').child(user_id).child('songs')
+        songs_ref.push().set(song_data)
 
-        # Retrieve and verify the hashed password from your database
-        user_data = retrieve_user_data(email)  # Implement this function
-        if bcrypt.checkpw(password.encode('utf-8'), user_data['hashed_password'].encode('utf-8')):
-            token = auth.create_custom_token(user.uid)
-            return {'success': True, 'message': 'User logged in successfully', 'token': token.decode('utf-8')}
-        else:
-            return {'success': False, 'message': 'Invalid password'}
+        return {'success': True, 'message': 'Song added successfully'}
     except Exception as e:
         return {'success': False, 'message': str(e)}
 
-@app.route('/login', methods=['POST'])
-def login():
+# Function to add songs from a text file
+def add_songs_from_file(user_id, file_path):
     try:
-        data = request.get_json()
-        email = data['email']
-        password = data['password']
+        with open(file_path, 'r') as file:
+            # Read each line from the file and add the song to the database
+            for line in file:
+                song_data = {'name': line.strip()}
+                add_song(user_id, song_data)
 
-        login_result = login_user(email, password)
+        return {'success': True, 'message': 'Songs added from file successfully'}
+    except Exception as e:
+        return {'success': False, 'message': str(e)}
 
-        return jsonify(login_result)
+# Function to add songs from a file upload
+def add_songs_from_upload(user_id, file):
+    try:
+        if file and allowed_file(file.filename):
+            # Save the uploaded file to the specified upload folder
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            # Read the file and add songs to the database
+            add_songs_from_file(user_id, file_path)
+
+            # Remove the uploaded file after processing (optional)
+            os.remove(file_path)
+
+            return {'success': True, 'message': 'Songs added from upload successfully'}
+        else:
+            return {'success': False, 'message': 'Invalid file format'}
 
     except Exception as e:
-        return jsonify({'success': False, 'message': str(e)})
+        return {'success': False, 'message': str(e)}
 
+# Endpoint to register a new user
 @app.route('/register', methods=['POST'])
 def register():
     try:
@@ -87,18 +103,6 @@ def register():
 
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
-
-# Function to add a single song manually
-def add_song(user_id, song_data):
-    try:
-        # Add the song data to the user's collection in the Firebase Realtime Database
-        database = firebase_admin.db.reference()
-        songs_ref = database.child('users').child(user_id).child('songs')
-        songs_ref.push().set(song_data)
-
-        return {'success': True, 'message': 'Song added successfully'}
-    except Exception as e:
-        return {'success': False, 'message': str(e)}
 
 # Endpoint to add a single song manually
 @app.route('/add-song', methods=['POST'])
@@ -118,19 +122,6 @@ def add_single_song():
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
 
-# Function to add songs from a text file
-def add_songs_from_file(user_id, file_path):
-    try:
-        with open(file_path, 'r') as file:
-            # Read each line from the file and add the song to the database
-            for line in file:
-                song_data = {'name': line.strip()}
-                add_song(user_id, song_data)
-
-        return {'success': True, 'message': 'Songs added from file successfully'}
-    except Exception as e:
-        return {'success': False, 'message': str(e)}
-
 # Endpoint to add songs from a text file
 @app.route('/add-songs-from-file', methods=['POST'])
 def add_songs_from_file_endpoint():
@@ -149,5 +140,30 @@ def add_songs_from_file_endpoint():
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
 
+# Endpoint to add songs from a file upload
+@app.route('/add-songs-from-upload', methods=['POST'])
+def add_songs_from_upload_endpoint():
+    try:
+        data = request.form
+        user_id = data['user_id']
+
+        # Check if the user provided a file in the request
+        if 'file' not in request.files:
+            return jsonify({'success': False, 'message': 'No file provided'})
+
+        file = request.files['file']
+
+        # Check if the file is allowed and add songs
+        file_addition_result = add_songs_from_upload(user_id, file)
+
+        if file_addition_result['success']:
+            return jsonify({'success': True, 'message': 'Songs added from upload successfully'})
+        else:
+            return jsonify({'success': False, 'message': file_addition_result['message']})
+
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
+
