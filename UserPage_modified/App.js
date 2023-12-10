e36a786472ef1557a9e72b0e8bbcb1aa32a47a4c
Mert Ali Çelik <117271708+mertaliclk@users.noreply.github.com>
2023-12-10 12:12:05 +0300

    Add files via upload

---
+++ UserPage_modified/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SongInputForm from "./Components/SongInputForm/SongInputForm";
+import BatchInputForm from "./Components/BatchInputForm/BatchInputForm";
+import UserPage from "./Components/UserPage/UserPage";
+
+function App() {
+  return (
+    <div>
+      <UserPage />
+    </div>
+  );
+}
+
+export default App;
